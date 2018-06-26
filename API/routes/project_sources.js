9245f397cd662492e99938a8eb90ab6d854a308d
Nachman Rosen <nachmanrosen@gmail.com>
2018-06-26 16:28:20 -0400

    remove

--- API/routes/project_sources.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-router.get('/', function(req, res, next) {
-  res.json({message: "the source of this project ishttp:// scotch.io/@devGson/api-authentication-with-json-web-tokensjwt-and-passport"});
-});
